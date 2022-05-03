718185f0d588aaaf5cf3ce39baa95bf3d15ea155
Vladislav Vanyuk <43759007+VladVanyuk@users.noreply.github.com>
2022-05-04 00:26:23 +0300

    pmwFan, dustSensor, receiverIR classes.  /data
    pmwFan
    dustSensor
    receiverIR 
     /data : index.html, style.css

--- WiFi_Dust_Cooler.ino
+++ WiFi_Dust_Cooler.ino
@@ -1,177 +1,192 @@
-#ifndef UNIT_TEST
+
+// Import required libraries
 #include <Arduino.h>
-#endif
-#include <IRremoteESP8266.h>
-#include <IRrecv.h>
-#include <IRutils.h>
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <FS.h>
+//#include "LittleFS.h"
+//#include <Arduino_JSON.h>
+//#include <Wire.h>
+#include "dustSensor.h"
+#include "receiverIR.h"
+#include "pwmFan.h"
 
-#include "index.h"  //Web page
+int density;
 
-uint16_t RECV_PIN = D6; // ИК-детектор
-IRrecv irrecv(RECV_PIN);
-decode_results results;
-
-const int pwmFanOutputPin = D7;   //blue wire
-const int relayPin1 = D2;
-const int relayPin2 = D3;
-float rpmFan = 63.75;    //255 = 100%  191.25 = 75%  127.5 = 50%  63.75 = 25%  0 = 0%
-float frequencyIncrement = 25.5;//12.75;   // incremental change in PWM frequency
-int timeFrequency = 100;    // time period the PWM frequency is changing
-bool firstFanIsActive;
-bool secondFanIsActive;
-
-void firstFanOFF();
-void firstFanON();
-void secondFanOFF();
-void secondFanON();
-
-//SSID and Password of your WiFi router
+// Replace with your network credentials
 const char* ssid = "AsusLyra";
 const char* password = "123456qwerty";
+
+// Stores LED state
+String fan1State;
+String fan2State;
+String automodState;
+
+// Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
-const char* PARAM_INPUT = "value";
-
-// Replaces placeholder with button section in your web page
-String processor(const String& var){
-  if (var == "SLIDERVALUE"){
-    String sliderValue = String(rpmFan, 2);
-    return sliderValue;
-  }
-  return String();
+String getDensity() {
+  density = dustSensor.readDustSensor();
+  Serial.println(density);
+  return String(density);
+}
+  
+String getRpm() {
+  float rpm = pwmFan.rpmFan;
+  Serial.println(rpm);
+  return String(rpm);
 }
 
-void setup()
-{
+// Replaces placeholder with LED state value
+String processor(const String& var){
+  Serial.println(var);
+  if(var == "STATEFAN1"){
+    if(digitalRead(pwmFan.relayPin1)){
+      fan1State = "ON";
+    }
+    else{
+      fan1State = "OFF";
+    }
+    Serial.print(fan1State);
+    return fan1State;
+  }
+  else if (var == "STATEFAN2"){
+    if(digitalRead(pwmFan.relayPin2)){
+      fan2State = "ON";
+    }
+    else{
+      fan2State = "OFF";
+    }
+    Serial.print(fan2State);
+    return fan2State;
+  }
+  else if (var == "DENSITY"){
+    return getDensity();
+  }
+  else if (var == "RPM"){
+    return getRpm();
+  }
+  else if (var == "AUTOMOD"){
+    if(receiverIR.autoMode == true){
+      automodState = "ON";
+    }
+    else{
+      automodState = "OFF";
+    }
+    Serial.print(automodState);
+    return automodState;
+  }
+}
+ 
+void setup(){
+  // Serial port for debugging purposes
   Serial.begin(115200);
-  pinMode(pwmFanOutputPin, OUTPUT);  
-  pinMode(relayPin1, OUTPUT);
-  pinMode(relayPin2, OUTPUT);
-  digitalWrite(relayPin1, LOW); //off
-  digitalWrite(relayPin2, LOW); // off
-  firstFanIsActive = false;
-  secondFanIsActive = false;
-  irrecv.enableIRIn();  // запускаем приемник
+  dustSensor.SETUP();
+  receiverIR.SETUP();
+  pwmFan.SETUP();
+  
+  // Initialize SPIFFS
+  if(!SPIFFS.begin()){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
 
-  WiFi.begin(ssid, password);     //Connect to your WiFi router
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.print(".");
+    Serial.println(".");
   }
-  //If connection successful show IP address in serial monitor
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
 
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
-  });  
-  server.on("/slider", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    String inputMessage;
-    // GET input1 value on <ESP_IP>/slider?value=<inputMessage>
-    if (request->hasParam(PARAM_INPUT)) {
-      inputMessage = request->getParam(PARAM_INPUT)->value();
-      rpmFan = inputMessage.toInt();                                              
-      analogWrite(pwmFanOutputPin, rpmFan);
-    }
-    else {
-      inputMessage = "No message sent";
-    }
-    Serial.println(inputMessage);
-    request->send(200, "text/plain", "OK");
+    request->send(SPIFFS, "/index.html", String(), false, processor);
   });
+  
+  // Route to load style.css file
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/style.css", "text/css");
+  });
+
+  // Route to set FAN1 to HIGH
+  server.on("/fan1on", HTTP_GET, [](AsyncWebServerRequest *request){
+    pwmFan.firstFanON();    
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+  
+  // Route to set FAN1 to LOW
+  server.on("/fan1off", HTTP_GET, [](AsyncWebServerRequest *request){
+    pwmFan.firstFanOFF();    
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+
+  // Route to set FAN2 to HIGH
+  server.on("/fan2on", HTTP_GET, [](AsyncWebServerRequest *request){
+    pwmFan.secondFanON();    
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+  
+  // Route to set FAN2 to LOW
+  server.on("/fan2off", HTTP_GET, [](AsyncWebServerRequest *request){
+    pwmFan.secondFanOFF();    
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+
+
+  server.on("/density", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", getDensity().c_str());
+  });
+  
+  server.on("/rpm", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", getRpm().c_str());
+  });
+
+   // Route to set Automod on
+  server.on("/automodon", HTTP_GET, [](AsyncWebServerRequest *request){
+    receiverIR.autoMode = true;   
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+  
+  // Route to set Automod off
+  server.on("/automodoff", HTTP_GET, [](AsyncWebServerRequest *request){
+    receiverIR.autoMode = false;   
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+
+  // Start server
   server.begin();
+  
 }
 
-void loop()
-{
-  analogWrite(pwmFanOutputPin, rpmFan);  
-  delay(timeFrequency);
-  receiver();
+ 
+void loop(){
+  receiverIR.receiver();
+  density = dustSensor.readDustSensor();
+  
+  if (receiverIR.autoMode == true){
+    if (density > 500){
+      pwmFan.firstFanON();
+      pwmFan.secondFanON();
+      pwmFan.rpmFan = density/4 - 1; 
+      analogWrite(pwmFan.pwmFanOutputPin, pwmFan.rpmFan);
+      Serial.print("WAIT 5 min");  
+      delay(300000);
+    } else if (density <= 500) {
+      pwmFan.firstFanOFF();
+      pwmFan.secondFanOFF();
+      delay(10000);
+      }
+  }
+   else if (receiverIR.autoMode == false){
+    receiverIR.receiver();
+    analogWrite(pwmFan.pwmFanOutputPin, pwmFan.rpmFan);  
+    delay(pwmFan.timeFrequency);
+     }
+  receiverIR.receiver();
+  delay(1000);
 }
-
-void firstFanOFF(){
-  digitalWrite(relayPin1, LOW);
-  firstFanIsActive = false;
-  }
-
-void firstFanON(){
-  digitalWrite(relayPin1, HIGH);
-  firstFanIsActive = true;
-  }
-
-void secondFanOFF(){
-  digitalWrite(relayPin1, LOW);
-  secondFanIsActive = false;
-  }
-
-void secondFanON(){
-  digitalWrite(relayPin1, HIGH);
-  secondFanIsActive = true;
-  }
-
-void receiver(){
-  if ( irrecv.decode( &results )) { // если данные пришли
-    switch ( results.value) {
-    case 3238126971://*
-        firstFanON();
-        break;
-    case 4039382595://#
-        firstFanOFF();
-        break;
-    case 2538093563://0
-        rpmFan = 0;
-        break;
-    case 3810010651: //1
-        rpmFan = 25.5;
-        break;
-    case 5316027: //2
-        rpmFan = 51;
-        break;
-    case 4001918335://3
-        rpmFan = 76.5; 
-        break;
-    case 1386468383://4
-        rpmFan = 102;
-        break;
-    case 3622325019://5
-        rpmFan = 127.5; //50%
-        break;
-    case 553536955://6
-        rpmFan = 153;
-        break;
-    case 4034314555://7
-        rpmFan = 178.5;
-        break;   
-    case 2747854299://8
-        rpmFan = 204;
-        break;
-    case 3855596927://9
-        rpmFan = 229.5; 
-        break;
-    case 1033561079:// ^
-        if (rpmFan >=0 || rpmFan <=255){ rpmFan = rpmFan + 5; }
-        else {rpmFan = 255;}
-        break;
-    case 465573243:// V
-        if (rpmFan >=0 || rpmFan <=255){ rpmFan = rpmFan - 5; }
-        else {rpmFan = 0;}
-        break;
-    case 2351064443:// <
-        if (rpmFan >=0 || rpmFan <=255){ rpmFan = rpmFan - 25.5; }
-        else {rpmFan = 0;}
-        break;
-    case 71952287:// >
-        if (rpmFan >=0 || rpmFan <=255){ rpmFan = rpmFan + 25.5; }
-        else {rpmFan = 255;}
-        break;   
-    case 1217346747:// ok
-        if(firstFanIsActive == true){ firstFanOFF(); }
-        else if(firstFanIsActive == false){ firstFanON(); }
-        break;                                           
-    }
-  irrecv.resume(); // принимаем следующую команду                  
-  }
- }
